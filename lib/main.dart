a6fefa9614c84f2edf8c819efeaa79ecc23edde2
cqhung1412 <wokhug99@gmail.com>
2021-08-08 02:10:12 +0700

    first app code

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,17 @@
-void main(List<String> args) {}
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyFirstApp());
+
+class MyFirstApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Fluttering"),
+        ),
+        body: Text("ThisIsMyFirstFlutter"),
+      ),
+    );
+  }
+}
