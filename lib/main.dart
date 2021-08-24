53dd81fca098e7cb4075b75264374a6c3ff70a5c
cqhung1412 <wokhug99@gmail.com>
2021-08-24 18:05:02 +0700

    stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,32 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyFirstApp());
+void main() => runApp(MyApp());
 
-class MyFirstApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+  var questions = [
+    'What\'s your most hated animal?',
+    'What\'s your favorite animal meat?'
+  ];
+
+  void answerQuestion() {
+    setState(() {
+      var newIndex = questionIndex;
+      if (questionIndex < questions.length - 1) {
+        newIndex = questionIndex + 1;
+      }
+      // change state
+      questionIndex = newIndex;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -10,7 +34,21 @@ class MyFirstApp extends StatelessWidget {
         appBar: AppBar(
           title: Text("Fluttering"),
         ),
-        body: Text("ThisIsMyFirstFlutter"),
+        body: Column(
+          children: <Widget>[
+            Text(
+              questions[questionIndex],
+            ),
+            ElevatedButton(
+              child: Text("Yes, I do"),
+              onPressed: () => answerQuestion(),
+            ),
+            ElevatedButton(
+              child: Text("No, I don't"),
+              onPressed: () => answerQuestion(),
+            ),
+          ],
+        ),
       ),
     );
   }
